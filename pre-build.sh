f87f6f467b0892afb949bc51de3ce1f8c9275e24
Michal Torma <michal.torma@nhm.uio.no>
2023-05-21 00:09:07 +0200

    pre build

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # sh ./install.sh -d
 flutter pub get
-cp tf_lib/android/* ./android/app/src/main/jniLibs
+cp -r tf_lib/android/* ./android/app/src/main/jniLibs
 cp -r tf_lib/ios/TensorFlowLiteC.framework ~/.pub-cache/hosted/pub.dartlang.org/tflite_flutter-0.9.0/ios
 cp -r tf_lib/ios/TensorFlowLiteCMetal.framework ~/.pub-cache/hosted/pub.dartlang.org/tflite_flutter-0.9.0/ios
 cp -r tf_lib/ios/tflite_flutter.podspec ~/.pub-cache/hosted/pub.dartlang.org/tflite_flutter-0.9.0/ios
